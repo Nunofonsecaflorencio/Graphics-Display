c1fd85f45c499547fbe6aa90c62d765318209b9f
22K <nunofonsecaflorencio@gmail.com>
2022-11-18 00:55:25 -0800

    Plotting Lines with Bresenham's line algorithm

--- src/visualizer/Screen.java
+++ src/visualizer/Screen.java
@@ -20,7 +20,7 @@ public class Screen extends JPanel implements ActionListener{
     
     private final int TILE_SIZE = 16;
     private final int LINE_SIZE = TILE_SIZE / 5;
-    private final int ROWS = 32, COLS = 32;
+    private final int ROWS = 48, COLS = 64;
     
     private final int WIDTH = (TILE_SIZE + LINE_SIZE) * COLS - LINE_SIZE;
     private final int HEIGHT = (TILE_SIZE + LINE_SIZE) * ROWS - LINE_SIZE;
@@ -39,6 +39,7 @@ public class Screen extends JPanel implements ActionListener{
         setBackground(TILE_OFF_COLOR);
         
         timer = new Timer(DELAY, this);
+        
     }
 
     @Override
@@ -55,17 +56,15 @@ public class Screen extends JPanel implements ActionListener{
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        drawLines(g2d);
+        drawGridLines(g2d);
         
-        for (int i = 0; i < COLS; i++) {
-            drawPoint(getLoc(i), getLoc(i / 2), g2d);
-        }
+        plotLine(4, 6, 60, 32, g2d);
         
         
         g2d.dispose();
     }
 
-    private void drawLines(Graphics2D g2d) {
+    private void drawGridLines(Graphics2D g2d) {
         g2d.setColor(LINE_COLOR);
         for (int i = 1; i < ROWS; i++) {
             int y = i * (TILE_SIZE + LINE_SIZE) - LINE_SIZE;
@@ -78,11 +77,68 @@ public class Screen extends JPanel implements ActionListener{
         }
     }
 
-    private void drawPoint(int i, int j, Graphics2D g2d) {
+    private void plotPoint(int i, int j, Graphics2D g2d) {
         g2d.setColor(TILE_ON_COLOR);
-        g2d.fillRect(i, j, TILE_SIZE, TILE_SIZE);
+        g2d.fillRect(getLoc(i), getLoc(j), TILE_SIZE, TILE_SIZE);
     }
-
+    
+    private void plotLineLow(int x0, int y0, int x1, int y1, Graphics2D g2d){
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int yi = 1;
+        if (dy < 0){
+            yi = -1;
+            dy = -dy;
+        }
+        int D = (2 * dy) - dx;
+        int y = y0;
+        
+        for (int x = x0; x <= x1; x++) {
+            plotPoint(x, y, g2d);
+            if (D > 0){
+                y = y + yi;
+                D = D + (2 * (dy - dx));
+            }else{
+                D = D + 2*dy;
+            }
+        }
+    }
+    
+    private void plotLineHigh(int x0, int y0, int x1, int y1, Graphics2D g2d){
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int xi = 1;
+        if (dx < 0){
+            xi = -1;
+            dx = -dx;
+        }
+        int D = (2 * dx) - dy;
+        int x = x0;
+        
+        for (int y = y0; y <= y1; y++) {
+            plotPoint(x, y, g2d);
+            if (D > 0){
+                x = x + xi;
+                D = D + (2 * (dx - dy));
+            }else{
+                D = D + 2*dx;
+            }
+        }
+    }
+    
+    private void plotLine(int x0, int y0, int x1, int y1, Graphics2D g2d){
+        if (Math.abs(y1 - y0) < Math.abs(x1 - x0)){
+            if (x0 > x1)
+                plotLineLow(x1, y1, x0, y0, g2d);
+            else
+                plotLineLow(x0, y0, x1, y1, g2d);
+        }else{
+            if (y0 > y1)
+                plotLineHigh(x1, y1, x0, y0, g2d);
+            else
+                plotLineHigh(x0, y0, x1, y1, g2d);
+        }
+    }
     
     private int getLoc(int i){
         return i * (TILE_SIZE + LINE_SIZE);
